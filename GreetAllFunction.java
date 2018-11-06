ebca31c3bb801c586ffb28f8158db8975193aff4
Nataliia Prokopenko <prokopenko.nataliya@gmail.com>
2018-11-06 22:14:22 +0200

    I added method and function SayHello

---
+++ GreetAllFunction.java
@@ -0,0 +1,14 @@
+public class GreetAllFunction{ 
+	public static void main (String...args) { 
+		for(String name:args) { 
+			String hello = sayHello(name);
+			System.out.println(hello);
+		} 
+	} 
+	
+	public static String sayHello(String helloName) {
+		String greeting = "Hello, "; 
+		String greetByName = greeting + helloName;
+		return greetByName;
+	}
+}
